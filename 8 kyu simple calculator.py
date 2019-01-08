d60a166c28409dfa9e916a8bf74e981452056ef6
Nagendra <44141606+nagendra226@users.noreply.github.com>
2019-01-08 09:14:11 +0530

    Create 8 kyu simple calculator.py
    You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
    
    Your function will accept three arguments:
    The first and second argument will be numbers.
    The third argument will represent a sign indicating the operation to perform on these two numbers.
    
    Example:
    calculator(1, 2, '+') => 3
    if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
    
    Example:
    calculator(1, 2, '$') # result will be "unknown value"
    Good luck!

---
+++ 8 kyu simple calculator.py
@@ -0,0 +1,11 @@
+def calculator(x,y,op):
+    if op == '+':
+        return x+y
+    elif op == '-':
+        return x-y
+    elif op == '*':
+        return x*y
+    elif op == '/':
+        return x/y
+    else:
+        return 'unknown value'

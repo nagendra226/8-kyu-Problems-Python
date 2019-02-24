56481b15dcb09067b5ca16305a31cd9af5ddd243
Nagendra <44141606+nagendra226@users.noreply.github.com>
2019-02-24 20:07:56 +0530

    Create 8Kyu - Color Ghost.py
    Color Ghost
    Create a class Ghost
    
    Ghost objects are instantiated without any arguments.
    
    Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
    
    ghost = Ghost()
    ghost.color  #=> "white" or "yellow" or "purple" or "red"

---
+++ 8Kyu - Color Ghost.py
@@ -0,0 +1,7 @@
+import random
+class Ghost():
+    # your code goes here
+    def __init__(self,color= ['white', 'yellow', 'purple', 'red']):
+        self.color = random.choice(color)
+      
+    
